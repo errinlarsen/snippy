9d0e8831255855bac9cb37f7b30ec8a156800ce3
Errin Larsen <elarsen@insightcards.com>
2011-09-19 14:11:51 -0500

    Modify env.rb for cucumber to change database_cleaners strategy for MongoDB

--- features/support/env.rb
+++ features/support/env.rb
@@ -31,8 +31,13 @@ ActionController::Base.allow_rescue = false
 
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
+# begin
+#   DatabaseCleaner.strategy = :transaction
+# rescue NameError
+#   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+# end
 begin
-  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
