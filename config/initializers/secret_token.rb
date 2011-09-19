1a26223632c7eeabb6b4f29d3cf6a4f252ddd951
Errin Larsen <elarsen@insightcards.com>
2011-09-19 09:51:32 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snippy::Application.config.secret_token = '56ec32d360a2a5dc04ef3ed9d263c4fc1225952c3780743b221a6c01a9c190ea55a7fd0e7a063015bbe74aae3c783fcd484253456381f2d603e44b4fb7356ea0'
